92956c9815f9cca3fab177ccb5848d428241be11
Esteban I. Ruiz Moreno <exio4.com@gmail.com>
2017-11-16 00:00:03 -0300

    Update build.sh
    Avoid recompiling build.run if it has been compiled already

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if ! [[ -x ./build.run ]]; then
+   echo "[.. compiling building manager ..]"
+   g++ -std=c++11 -o build.run build.cxx -O1
+fi
 
-g++ -std=c++11 -o build.run build.cxx -O1 && ./build.run $@
+exec ./build.run $@
